b73e47a5986ab3b887ed96da7269d59a9905cfb3
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-07-01 18:53:14 +0300

    Add files via upload

---
+++ Introduction/EmployeesFullInformation.cs
@@ -0,0 +1,31 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new SoftUniContext();
+
+            GetEmployeesFullInformation(dbContext); 
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+            
+            var employees = context.Employees.ToList().OrderBy(x => x.EmployeeId);
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
