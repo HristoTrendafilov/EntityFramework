582fb3c3be4e96cb9e65131303912b97f1d5076b
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-07-01 19:02:30 +0300

    Add files via upload

---
+++ Introduction/EmployeesWithSalaryOver50000.cs
@@ -0,0 +1,34 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(dbContext));
+        }
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(x => x.Salary > 50000)
+                .OrderBy(x => x.FirstName)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
