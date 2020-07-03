20651853c7f51539f8ab1344384c9468ec514542
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-07-03 19:52:57 +0300

    Add files via upload

---
+++ Introduction/FindLast10Projects.cs
@@ -0,0 +1,44 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new SoftUniContext();
+
+            Console.WriteLine(GetLatestProjects(dbContext));
+        }
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var projects = context.Projects
+                .OrderByDescending(x => x.StartDate)
+                .Take(10)
+                .OrderBy(x => x.Name)
+                .Select(x => new
+                {
+                    x.Name,
+                    x.Description,
+                    StartDate = x.StartDate.ToString("M/d/yyyy h:mm:ss tt")
+                }).ToList();
+
+            foreach (var project in projects)
+            {
+                sb.AppendLine($"{project.Name}");
+                sb.AppendLine($"{project.Description}");
+                sb.AppendLine($"{project.StartDate}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
