9cd9ebf67548d0f4ed27418ab5f938eef18c3a32
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-07-03 17:40:44 +0300

    Add files via upload

---
+++ Introduction/EmployeesFromResearchAndDevelopment.cs
@@ -0,0 +1,42 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(dbContext));
+        }
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(x => x.Department.Name == "Research and Development")
+                .Select(x=> new 
+                {
+                  x.FirstName,
+                  x.LastName,
+                  DepartmentName = x.Department.Name,
+                  x.Salary
+                })
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName} - ${employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
