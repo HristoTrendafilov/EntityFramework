55b84739c35b9becf53beeabad8acc8131dcd6f8
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-07-03 20:04:58 +0300

    Add files via upload

---
+++ Introduction/IncreaseSalaries.cs
@@ -0,0 +1,46 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new SoftUniContext();
+
+            Console.WriteLine(IncreaseSalaries(dbContext));
+        }
+        public static string IncreaseSalaries(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Engineering"
+                         || e.Department.Name == "Tool Design"
+                         || e.Department.Name == "Marketing"
+                         || e.Department.Name == "Information Services")
+                .OrderBy(e => e.FirstName).ThenBy(e => e.LastName)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                employee.Salary *= 1.12M;
+            }
+
+            context.SaveChanges();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} (${employee.Salary:F2})");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
