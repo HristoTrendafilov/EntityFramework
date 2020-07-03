d2d65936957922ea36f09fa1996df1c439466c88
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-07-03 19:31:06 +0300

    Add files via upload

---
+++ Introduction/Employee147.cs
@@ -0,0 +1,45 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new SoftUniContext();
+
+            Console.WriteLine(GetEmployee147(dbContext));
+        }
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employee147 = context.Employees.Where(x => x.EmployeeId == 147)
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    x.JobTitle,
+                    Projects = x.EmployeesProjects.Select(ep => new 
+                    {
+                        Name = ep.Project.Name
+                    }).OrderBy(x => x.Name).ToList()
+                }).Single();
+
+            sb.AppendLine($"{employee147.FirstName} {employee147.LastName} - {employee147.JobTitle}");
+
+            foreach (var project in employee147.Projects)
+            {
+                sb.AppendLine($"{project.Name}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
