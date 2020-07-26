6498939bed32fc39c75130a8a39f74dff22a7bba
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-07-26 16:39:40 +0300

    Add files via upload

---
+++ XML/CarDealer/DTO/ExportSaleDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO
+{
+    [XmlType("Sale")]
+    public class ExportSaleDTO
+    {
+        [XmlElement("car")]
+        public ExportCarDto Car { get; set; }
+
+        [XmlElement("discount")]
+        public decimal Discount { get; set; }
+
+        [XmlElement("customer-name")]
+        public string CustomerName { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+        [XmlElement("price-with-discount")]
+        public decimal PriceWithDiscount { get; set; }
+    }
+    [XmlType("car")]
+    public class ExportCarDto
+    {
+        [XmlAttribute("make")]
+        public string Make { get; set; }
+
+        [XmlAttribute("model")]
+        public string Model { get; set; }
+
+        [XmlAttribute("travelledDistance")]
+        public long TravelledDistance { get; set; }
+    }
+}
---
+++ XML/CarDealer/DTO/ImportCarDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO
+{
+    [XmlType("Car")]
+    public class ImportCarDTO
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("TraveledDistance")]
+        public long TravelledDistance { get; set; }
+
+        [XmlArray("parts")]
+        public ImportCarPartsDto[] Parts { get; set; }
+    }
+
+    [XmlType("partId")]
+    public class ImportCarPartsDto
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+    }
+}
