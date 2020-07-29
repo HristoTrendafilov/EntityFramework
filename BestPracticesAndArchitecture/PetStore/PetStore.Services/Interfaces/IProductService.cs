e58f387f420201afb090b2aa0a4b01d71ffc76f0
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-07-29 21:50:40 +0300

    Add files via upload

---
+++ BestPracticesAndArchitecture/PetStore/PetStore.Services/Interfaces/IProductService.cs
@@ -0,0 +1,25 @@
+﻿using PetStore.ServiceModels.Products.InputModels;
+using PetStore.ServiceModels.Products.OutputModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStore.Services.Interfaces
+{
+    public interface IProductService
+    {
+        ICollection<ListAllProductsByProductTypeServiceModel> ListAllByProductType(string type);
+
+        void AddProduct(AddProductInputServiceModel model);
+
+        ICollection<ListAllProductsServiceModel> GetAll();
+
+        bool RemoveById(string id);
+
+        bool RemoveByName(string name);
+
+        ICollection<ListAllProductsByNameServiceModel> SearchByName(string name, bool caseSensitive);
+
+        void EditProduct(string id, EditProductInputServiceModel model);
+    }
+}
