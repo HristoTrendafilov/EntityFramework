c8bc3d391dbc563da425646ff1bb3b2e10f88008
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-07-29 21:48:30 +0300

    Add files via upload

---
+++ BestPracticesAndArchitecture/PetStore/PetStore.Data/Configurations/ClientEntityConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetStore.Common;
+using PetStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStore.Data.Configurations
+{
+    public class ClientEntityConfiguration : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.Property(x => x.Username)
+                .HasMaxLength(GlobalConstants.UserNameMaxLength)
+                .IsUnicode(false);
+
+            builder.Property(x => x.Email)
+                .HasMaxLength(GlobalConstants.EmailMaxLength)
+                .IsUnicode(false);
+
+            builder.Property(x => x.FirstName)
+                .HasMaxLength(GlobalConstants.ClientNameMaxLength)
+                .IsUnicode(true);
+
+            builder.Property(x => x.LastName)
+                .HasMaxLength(GlobalConstants.ClientNameMaxLength)
+                .IsUnicode(true);
+        }
+    }
+}
---
+++ BestPracticesAndArchitecture/PetStore/PetStore.Data/Configurations/ProductEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetStore.Common;
+using PetStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStore.Data.Configurations
+{
+    public class ProductEntityConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasAlternateKey(x => x.Name);
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(GlobalConstants.ProductNameMaxLength)
+                .IsUnicode(true);
+        }
+    }
+}
