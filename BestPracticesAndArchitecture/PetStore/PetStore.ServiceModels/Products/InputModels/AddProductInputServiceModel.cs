06d1fd8a1f2d2f05cc8a4dd1962c511709457b61
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-08-01 10:50:43 +0300

    Add files via upload

---
+++ BestPracticesAndArchitecture/PetStore/PetStore.ServiceModels/Products/InputModels/AddProductInputServiceModel.cs
@@ -0,0 +1,22 @@
+﻿using PetStore.Common;
+using PetStore.Models.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PetStore.ServiceModels.Products.InputModels
+{
+    public class AddProductInputServiceModel
+    {
+        [Required]
+        [MinLength(GlobalConstants.ProductNameMinLength)]
+        [MaxLength(GlobalConstants.ProductNameMaxLength)]
+        public string Name { get; set; }
+
+        public string ProductType { get; set; }
+
+        [Range(GlobalConstants.SellableMinPrice, double.MaxValue)]
+        public decimal Price { get; set; }
+    }
+}
