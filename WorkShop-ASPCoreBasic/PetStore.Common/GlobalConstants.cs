dfc5cfe6cbf21077154570f0ecf28a0bf3f2cc6b
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-08-01 15:26:09 +0300

    Add files via upload

---
+++ WorkShop-ASPCoreBasic/PetStore.Common/GlobalConstants.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStore.Common
+{
+    public static class GlobalConstants
+    {
+        //Breed
+        public const int BreedNameMinLength = 5;
+        public const int BreedNameMaxLength = 30;
+        
+        //Client
+        public const int UserNameMinLength = 6;
+        public const int UserNameMaxLength = 30;
+        public const int EmailMinLength = 6;
+        public const int EmailMaxLength = 50;
+        public const int ClientNameMinLength = 3;
+        public const int ClientNameMaxLength = 50;
+
+        //ClientProduct
+        public const int MinQuantity = 1;
+
+        //Order
+        public const int TownNameMinLength = 3;
+        public const int TownNameMaxLength = 40;
+        public const int AddressTextMinLength = 5;
+        public const int AddressTextMaxLength = 70;
+
+        //Pet
+        public const int PetNameMinLength = 3;
+        public const int PetNameMaxLength = 50;
+        public const int MinPetAge = 0;
+        public const int MaxPetAge = 200;
+
+        //Product
+        public const int ProductNameMinLength = 3;
+        public const int ProductNameMaxLength = 50;
+
+        //SellingPrice
+        public const int SellableMinPrice = 0;
+    }
+}
---
+++ WorkShop-ASPCoreBasic/PetStore.Data/Configurations/PetEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetStore.Common;
+using PetStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStore.Data.Configurations
+{
+    public class PetEntityConfiguration : IEntityTypeConfiguration<Pet>
+    {
+        public void Configure(EntityTypeBuilder<Pet> builder)
+        {
+            builder.Property(x => x.Name)
+                .HasMaxLength(GlobalConstants.PetNameMaxLength)
+                .IsUnicode(true);
+        }
+    }
+}
